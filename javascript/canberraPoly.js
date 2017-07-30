f538cadfff9b1b445c5dae4f04c3f334f21dc9b0
Sam West-Sooby <s.west.sooby@gmail.com>
2017-07-30 16:34:33 +1000

    added task page

--- javascript/canberraPoly.js
+++ javascript/canberraPoly.js
@@ -196,87 +196,85 @@ function initMap() {
         center: canberra
     });
 
-directionsDisplay.setMap(map);
+// directionsDisplay.setMap(map);
 
-calculateAndDisplayRoute(directionsService, directionsDisplay);
+// calculateAndDisplayRoute(directionsService, directionsDisplay);
 
-          // Define the LatLng coordinates for the polygon's path.
-  //       var macquarie = [
-  //         {lat: -35.243646, lng: 149.056215},
-  //         {lat: -35.247391, lng: 149.056156},
-  //         {lat: -35.254801, lng: 149.054063},
-  //         {lat: -35.257224, lng: 149.054407},
-  //         {lat: -35.255253, lng: 149.060855},
-  //         {lat: -35.255253, lng: 149.061860},
-  //         {lat: -35.256107, lng: 149.066084},
-  //         {lat: -35.255564, lng: 149.069631},
-  //         {lat: -35.255573, lng: 149.069360},
-  //         {lat: -35.253411, lng: 149.072922},
-  //         {lat: -35.253514, lng: 149.074530},
-  //         {lat: -35.250322, lng: 149.075759},
-  //         {lat: -35.246486, lng: 149.063748},
-  //         {lat: -35.243646, lng: 149.056215}
-  //       ];
+        //   Define the LatLng coordinates for the polygon's path.
+        var macquarie = [
+          {lat: -35.243646, lng: 149.056215},
+          {lat: -35.247391, lng: 149.056156},
+          {lat: -35.254801, lng: 149.054063},
+          {lat: -35.257224, lng: 149.054407},
+          {lat: -35.255253, lng: 149.060855},
+          {lat: -35.255253, lng: 149.061860},
+          {lat: -35.256107, lng: 149.066084},
+          {lat: -35.255564, lng: 149.069631},
+          {lat: -35.255573, lng: 149.069360},
+          {lat: -35.253411, lng: 149.072922},
+          {lat: -35.253514, lng: 149.074530},
+          {lat: -35.250322, lng: 149.075759},
+          {lat: -35.246486, lng: 149.063748},
+          {lat: -35.243646, lng: 149.056215}
+        ];
 
-  //       var duffy = [
-  //         {lat: -35.325319, lng: 149.026013},
-  //         {lat: -35.337209, lng: 149.022382},
-  //         {lat: -35.344438, lng: 149.024731},
-  //         {lat: -35.344003, lng: 149.027187},
-  //         {lat: -35.343785, lng: 149.031351},
-  //         {lat: -35.342392, lng: 149.034608},
-  //         {lat: -35.342348, lng: 149.041121},
-  //         {lat: -35.341477, lng: 149.040854},
-  //         {lat: -35.336686, lng: 149.037331},
-  //         {lat: -35.334857, lng: 149.037704},
-  //         {lat: -35.332810, lng: 149.039947},
-  //         {lat: -35.330197, lng: 149.041281},
-  //         {lat: -35.329718, lng: 149.043630},
-  //         {lat: -35.325188, lng: 149.043951},
-  //         {lat: -35.326930, lng: 149.040801},
-  //         {lat: -35.326451, lng: 149.031885},
-  //         {lat: -35.325319, lng: 149.026013}
-  //       ];
+        var duffy = [
+          {lat: -35.325319, lng: 149.026013},
+          {lat: -35.337209, lng: 149.022382},
+          {lat: -35.344438, lng: 149.024731},
+          {lat: -35.344003, lng: 149.027187},
+          {lat: -35.343785, lng: 149.031351},
+          {lat: -35.342392, lng: 149.034608},
+          {lat: -35.342348, lng: 149.041121},
+          {lat: -35.341477, lng: 149.040854},
+          {lat: -35.336686, lng: 149.037331},
+          {lat: -35.334857, lng: 149.037704},
+          {lat: -35.332810, lng: 149.039947},
+          {lat: -35.330197, lng: 149.041281},
+          {lat: -35.329718, lng: 149.043630},
+          {lat: -35.325188, lng: 149.043951},
+          {lat: -35.326930, lng: 149.040801},
+          {lat: -35.326451, lng: 149.031885},
+          {lat: -35.325319, lng: 149.026013}
+        ];
 
-  //       var forrest = [
-  //         {lat: -35.319527, lng: 149.114355},
-  //         {lat: -35.323168, lng: 149.119634},
-  //         {lat: -35.322188, lng: 149.123024},
-  //         {lat: -35.322783, lng: 149.124312},
-  //         {lat: -35.322643, lng: 149.126586},
-  //         {lat: -35.321873, lng: 149.127917},
-  //         {lat: -35.320752, lng: 149.128689},
-  //         {lat: -35.319176, lng: 149.133024},
-  //         {lat: -35.317286, lng: 149.132895},
-  //         {lat: -35.316515, lng: 149.134182},
-  //         {lat: -35.316095, lng: 149.135341},
-  //         {lat: -35.314834, lng: 149.133367},
-  //         {lat: -35.313434, lng: 149.133238},
-  //         {lat: -35.310772, lng: 149.128646},
-  //         {lat: -35.311823, lng: 149.126887},
-  //         {lat: -35.312383, lng: 149.124483},
-  //         {lat: -35.319527, lng: 149.114355}
-  //       ];
+        var forrest = [
+          {lat: -35.319527, lng: 149.114355},
+          {lat: -35.323168, lng: 149.119634},
+          {lat: -35.322188, lng: 149.123024},
+          {lat: -35.322783, lng: 149.124312},
+          {lat: -35.322643, lng: 149.126586},
+          {lat: -35.321873, lng: 149.127917},
+          {lat: -35.320752, lng: 149.128689},
+          {lat: -35.319176, lng: 149.133024},
+          {lat: -35.317286, lng: 149.132895},
+          {lat: -35.316515, lng: 149.134182},
+          {lat: -35.316095, lng: 149.135341},
+          {lat: -35.314834, lng: 149.133367},
+          {lat: -35.313434, lng: 149.133238},
+          {lat: -35.310772, lng: 149.128646},
+          {lat: -35.311823, lng: 149.126887},
+          {lat: -35.312383, lng: 149.124483},
+          {lat: -35.319527, lng: 149.114355}
+        ];
 
  
 
-  //       // Construct the polygon.
-  //       var canberraPoly = new google.maps.Polygon({
-  //         paths: [macquarie, duffy, forrest],
-  //         strokeColor: '#FF0000',
-  //         strokeOpacity: 0.8,
-  //         strokeWeight: 2,
-  //         fillColor: '#FF0000',
-  //         fillOpacity: 0.35
-  //       });
-  //       canberraPoly.setMap(map);
+        // Construct the polygon.
+        var canberraPoly = new google.maps.Polygon({
+          paths: [macquarie, duffy, forrest],
+          strokeColor: '#FF0000',
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: '#FF0000',
+          fillOpacity: 0.35
+        });
+        canberraPoly.setMap(map);
 
 
-  // google.maps.event.addListener(canberraPoly, 'click', function (event) {
-  //     directionsDisplay.setMap(map);
-  //     calculateAndDisplayRoute(directionsService, directionsDisplay);
-  //     canberraPoly.setMap(null)
-  // });  
+  google.maps.event.addListener(canberraPoly, 'click', function (event) {
+         window.location = '../templates/task.html'
+  });  
 ///// marker adding
   // var marker, i;
   // var markers = [];
